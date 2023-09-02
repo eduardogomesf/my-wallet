d379e73d56f656451f7d3404c3d36b0264d385bc
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-02 16:34:55 -0300

    test: add unit tests for transaction controller

---
+++ test/transaction/unit/controller/transaction.spec.ts
@@ -0,0 +1,52 @@
+import { BadRequestException } from '@nestjs/common';
+import { TransactionType } from '../../../../src/shared';
+import { TransactionController } from '../../../../src/transaction/controller';
+import { CreateTransactionUseCase } from '../../../../src/transaction/use-case';
+
+const makeCreateTransactionUseCase = (): CreateTransactionUseCase => {
+  return {
+    create: jest.fn().mockResolvedValue({
+      ok: true,
+    }),
+  } as any;
+};
+
+describe('Transaction Controller', () => {
+  let sut: TransactionController;
+  let createTransactionUseCase: CreateTransactionUseCase;
+
+  beforeEach(() => {
+    createTransactionUseCase = makeCreateTransactionUseCase();
+    sut = new TransactionController(createTransactionUseCase);
+  });
+
+  it('Should return 400 if use case returns nok', async () => {
+    jest.spyOn(createTransactionUseCase, 'create').mockResolvedValueOnce({
+      ok: false,
+      error: { message: 'any', code: 'any_code' },
+    });
+
+    const promise = sut.create({
+      userId: 'any-id',
+      amount: 500,
+      type: TransactionType.CREDIT,
+    });
+
+    await expect(promise).rejects.toThrowError(
+      new BadRequestException({
+        message: 'any',
+        code: 'any_code',
+      }),
+    );
+  });
+
+  it('Should return 201 if use case returns ok', async () => {
+    const result = await sut.create({
+      userId: 'any-id',
+      amount: 500,
+      type: TransactionType.CREDIT,
+    });
+
+    expect(result).toBeNull();
+  });
+});
