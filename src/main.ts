1d8a601a0b50a6f6699d8c29886c792b9026d251
eduardogomesf <eduardogoomesf@gmail.com>
2023-09-03 13:25:59 -0300

    feat: add swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import * as swaggerUi from 'swagger-ui-express';
+import * as YAML from 'yamljs';
 import { AppModule } from './app.module';
 import { ENVS } from './shared/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerDocument = YAML.load('./docs/swagger.yaml');
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   await app.listen(ENVS.APP.PORT, () => {
     console.log(`Server running on port ${ENVS.APP.PORT}`);
   });
